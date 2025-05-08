8a4a3392a7b086fe80fa13384c47fa234bd27dc9
zhongyujie <3567878896@qq.com>
2025-05-08 21:44:15 +0800

    管理端功能开发，数据采集链路完成

---
+++ Dynamic-Thread-Pool-Admin/src/main/java/org/example/trigger/DynamicThreadPoolController.java
@@ -0,0 +1,110 @@
+package org.example.trigger;
+
+
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.example.domain.model.entity.ThreadPoolConfigEntity;
+import org.example.types.Response;
+import org.redisson.api.RSet;
+import org.redisson.api.RTopic;
+import org.redisson.api.RedissonClient;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Slf4j
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/v1/dynamic/thread/pool/")
+public class DynamicThreadPoolController {
+
+    @Resource
+    public RedissonClient redissonClient;
+
+    /**
+     * 查询线程池数据
+     * curl --request GET \
+     * --url 'http://localhost:8089/api/v1/dynamic/thread/pool/query_thread_pool_list'
+     */
+    @GetMapping(value = "query_thread_pool_list")
+    public Response<List<ThreadPoolConfigEntity>> queryThreadPoolList() {
+        try {
+            RSet<ThreadPoolConfigEntity> set = redissonClient.getSet("THREAD_POOL_CONFIG_LIST_KEY");
+
+            List<ThreadPoolConfigEntity> threadPoolList = new ArrayList<>(set.readAll());
+            return Response.<List<ThreadPoolConfigEntity>>builder()
+                    .code(Response.Code.SUCCESS.getCode())
+                    .info(Response.Code.SUCCESS.getInfo())
+                    .data(threadPoolList)
+                    .build();
+        } catch (Exception e) {
+            log.error("查询线程池数据异常", e);
+            return Response.<List<ThreadPoolConfigEntity>>builder()
+                    .code(Response.Code.UN_ERROR.getCode())
+                    .info(Response.Code.UN_ERROR.getInfo())
+                    .build();
+        }
+    }
+
+    /**
+     * 查询线程池配置
+     * curl --request GET \
+     * --url 'http://localhost:8089/api/v1/dynamic/thread/pool/query_thread_pool_config?appName=dynamic-thread-pool-test-app&threadPoolName=threadPoolExecutor'
+     */
+    @GetMapping(value = "query_thread_pool_config")
+    public Response<ThreadPoolConfigEntity> queryThreadPoolConfig(@RequestParam String appName, @RequestParam String threadPoolName) {
+        try {
+            String cacheKey = "THREAD_POOL_CONFIG_PARAMETER_LIST_KEY" + "_" + appName + "_" + threadPoolName;
+            ThreadPoolConfigEntity threadPoolConfigEntity = redissonClient.<ThreadPoolConfigEntity>getBucket(cacheKey).get();
+            return Response.<ThreadPoolConfigEntity>builder()
+                    .code(Response.Code.SUCCESS.getCode())
+                    .info(Response.Code.SUCCESS.getInfo())
+                    .data(threadPoolConfigEntity)
+                    .build();
+        } catch (Exception e) {
+            log.error("查询线程池配置异常", e);
+            return Response.<ThreadPoolConfigEntity>builder()
+                    .code(Response.Code.UN_ERROR.getCode())
+                    .info(Response.Code.UN_ERROR.getInfo())
+                    .build();
+        }
+    }
+
+    /**
+     * 修改线程池配置
+     * curl --request POST \
+     * --url http://localhost:8089/api/v1/dynamic/thread/pool/update_thread_pool_config \
+     * --header 'content-type: application/json' \
+     * --data '{
+     * "appName":"dynamic-thread-pool-test-app",
+     * "threadPoolName": "threadPoolExecutor",
+     * "corePoolSize": 1,
+     * "maximumPoolSize": 10
+     * }'
+     */
+    @PostMapping(value = "update_thread_pool_config")
+    public Response<Boolean> updateThreadPoolConfig(@RequestBody ThreadPoolConfigEntity request) {
+        try {
+            log.info("修改线程池配置开始 {} {} {}", request.getAppName(), request.getThreadPoolName(), JSON.toJSONString(request));
+            RTopic topic = redissonClient.getTopic("DYNAMIC_THREAD_POOL_REDIS_TOPIC" + "_" + request.getAppName());
+            topic.publish(request);
+            log.info("修改线程池配置完成 {} {}", request.getAppName(), request.getThreadPoolName());
+            return Response.<Boolean>builder()
+                    .code(Response.Code.SUCCESS.getCode())
+                    .info(Response.Code.SUCCESS.getInfo())
+                    .data(true)
+                    .build();
+        } catch (Exception e) {
+            log.error("修改线程池配置异常 {}", JSON.toJSONString(request), e);
+            return Response.<Boolean>builder()
+                    .code(Response.Code.UN_ERROR.getCode())
+                    .info(Response.Code.UN_ERROR.getInfo())
+                    .data(false)
+                    .build();
+        }
+    }
+
+}
